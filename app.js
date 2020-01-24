ba8214df105397a929fa86fa53138f8f47301ba7
beata <beata@emc2wave.com>
2020-01-23 22:01:24 -0800

    select id and values

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const form = document.querySelector("#signup-form");
 
+const creditCard = document.querySelector("#cc");
+const terms = document.querySelector("#terms");
+const fruitsSelect = document.querySelector("#fruits");
+
 form.addEventListener("submit", function(e) {
   alert("Hello");
-  console.log(e);
+  console.log("cc :", creditCard.value);
+  console.log("termc :", terms.checked);
+  console.log("fruits :", fruitsSelect.value);
   e.preventDefault();
 });
